968a915f6d8e1c70cba838eae2237f937f0840cc
v_vhaotang <v_vhaotang@tencent.com>
2020-04-02 18:22:26 +0800

    feat: webpack管理资源结束时文件夹状态

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
+const path = require('path');
+
 module.exports = {
-    entry: './path/to/my/entry/file.js'
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+            'file-loader'
+        ]
+      },
+      {
+          test: /\.(woff|woff2|ttf|otf)$/,
+          use: [
+              'file-loader'
+          ]
+      }
+    ]
+  }
 };

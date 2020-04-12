44cffdc519a7367104e65e65d2e3265b0b19d759
zhiyouth <13243071208.com>
2020-04-12 14:17:58 +0800

    feat: nodejs代理服务器

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const config = require('./webpack.prod.js');
@@ -11,6 +12,12 @@ const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath
 }));
+
+app.use('/api', createProxyMiddleware({
+  target: "http://106.54.155.93:8080",
+  changeOrigin: true,     // target是域名的话，需要这个参数，
+  pathRewrite: {"^/api" : ""}
+}))
 
 // Serve the files on port 80.
 app.listen(80, function () {
